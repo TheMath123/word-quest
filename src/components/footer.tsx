fb32593aec34e51e217279d910e7e6dee32104de
Matheus P Agostinho <contact@matheuspa.com>
2024-10-02 11:32:21 -0300

    separte footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return <footer className="flex flex-col items-center justify-center">
+    <span className="text-gray-300 text-sm my-4">
+      @2024 Word Quest. All rights reserved.
+    </span>
+  </footer>
+}
