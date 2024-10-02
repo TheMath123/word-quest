5cd5f20021322fb96a3b8dcf5f3de81256eb9a4c
Matheus P Agostinho <contact@matheuspa.com>
2024-10-02 09:54:16 -0300

    fix typing and sava data and parse data

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -3,42 +3,54 @@ import { getEncryptionKey, encrypt, decrypt, encryptKey } from "../lib/crypto";
 
 const isBrowser = typeof window !== "undefined";
 
-export const useLocalStorage = <T>(key: string, initialValue?: T) => {
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T | ((val: T) => T)) => void];
+export function useLocalStorage<T = undefined>(
+  key: string
+): [T | undefined, (value: T | ((val: T | undefined) => T)) => void];
+
+export function useLocalStorage<T>(key: string, initialValue?: T) {
   const encryptionKey = React.useMemo(
     () => (isBrowser ? getEncryptionKey() : null),
     []
   );
 
-  React.useEffect(() => {
-    setStoredValue(getValue());
-  }, []);
-
   const getValue = React.useCallback(() => {
+    if (!isBrowser || !encryptionKey) return initialValue;
+
     const encryptedKey = encryptKey(key);
     const item = localStorage.getItem(encryptedKey);
-    if (item && encryptionKey) {
-      const decryptedItem = decrypt(item, encryptionKey);
-      return JSON.parse(decryptedItem);
-    } else if (initialValue) {
-      return initialValue;
+
+    if (item) {
+      try {
+        const decryptedItem = decrypt(item, encryptionKey);
+        return JSON.parse(decryptedItem);
+      } catch (error) {
+        console.error("Error parsing stored item:", error);
+        return initialValue;
+      }
     }
-    return undefined;
+
+    return initialValue;
   }, [encryptionKey, initialValue, key]);
 
-  const [storedValue, setStoredValue] = React.useState<T>(() => {
-    if (!isBrowser) {
-      return initialValue ?? undefined;
-    }
+  const [storedValue, setStoredValue] = React.useState<T | undefined>(() => {
     try {
       return getValue();
     } catch (error) {
-      console.error(error);
+      console.error("Error getting initial value:", error);
       return initialValue;
     }
   });
 
+  React.useEffect(() => {
+    setStoredValue(getValue());
+  }, []);
+
   const setValue = React.useCallback(
-    (value: T | ((val: T) => T)) => {
+    (value: T | ((val: T | undefined) => T)) => {
       if (!isBrowser || !encryptionKey) return;
       try {
         const valueToStore =
@@ -51,11 +63,11 @@ export const useLocalStorage = <T>(key: string, initialValue?: T) => {
         );
         localStorage.setItem(encryptedKey, encryptedValue);
       } catch (error) {
-        console.error(error);
+        console.error("Error setting value:", error);
       }
     },
     [key, storedValue, encryptionKey]
   );
 
   return [storedValue, setValue] as const;
-};
+}
