882cd110ac9fd4f98fb98579a46410e4a4387b5a
Matheus P Agostinho <contact@matheuspa.com>
2024-10-03 20:16:34 -0300

    create profile button componet

---
+++ src/components/profile-button/profile-button.tsx
@@ -0,0 +1,55 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getInitials } from "@/utils/get-initials";
+import { User } from "next-auth";
+import Link from "next/link";
+
+interface ProfileButtonProps {
+  user: User;
+}
+
+export function ProfileButton({ user }: ProfileButtonProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        {" "}
+        <Avatar>
+          <AvatarImage src={user.image ?? ""} />
+          <AvatarFallback>
+            {getInitials(user.name ?? "")}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          asChild
+        >
+          <Link
+            href='/profile'
+          >
+            Profile
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          asChild
+          className="text-red-500 hover:text-red-600"
+        >
+          <Link
+            href="/api/auth/signout"
+          >
+            Sign Out
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu >
+  );
+}
