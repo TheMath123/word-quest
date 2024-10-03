edcefa72baa35dd8b5a6aead05f2c4f465d26062
Matheus P Agostinho <contact@matheuspa.com>
2024-10-03 20:15:03 -0300

    create logout page

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "@/components";
+
+
+export default function Logout() {
+  return (
+    <div className="flex flex-col h-dvh">
+      <main className="grid place-content-center h-full">
+
+      </main>
+      <Footer />
+    </div>
+  );
+}
