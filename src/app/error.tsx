aec3f0d29387a5b533f55ff3b24125d406601f86
Matheus P Agostinho <contact@matheuspa.com>
2024-10-05 17:21:01 -0300

    new styles for error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -12,10 +12,10 @@ export default function Error({ error, reset }: ErrorProps) {
 
   return (
     <div className="flex flex-col h-dvh">
-      <main className="grid place-content-center h-full">
-        <h1>_I m@ 3rr0!</h1>
-        <h2>W3 h@v3 a b1g pr0blem!</h2>
-        <h3>{error.message}</h3>
+      <main className="grid place-content-center h-full space-y-6">
+        <h1 className='font-bold text-2xl text-red-600'>_I @m 3rr0!</h1>
+        <h2 className='font-semibold text-xl text-yellow-600'>W3 h@v3 a b1g pr0blem!</h2>
+        <code className='font-medium text-lg max-w-lg text-gray-400 bg-gray-800 p-4 rounded-md'>{error.message}</code>
         <Button
           onClick={
             () => reset()
