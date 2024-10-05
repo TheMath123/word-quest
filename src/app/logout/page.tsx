24c2fdbed9c3d53eaef39840cb861a8217255aea
Matheus P Agostinho <contact@matheuspa.com>
2024-10-05 15:49:41 -0300

    create logout and action page

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,11 +1,13 @@
+import logout from "@/actions/logout";
 import { Footer } from "@/components";
 
+export default async function Logout() {
+  const rest = await logout()
 
-export default function Logout() {
   return (
     <div className="flex flex-col h-dvh">
       <main className="grid place-content-center h-full">
-
+        <h1 className="text-2xl font-bold">Good bye! =)</h1>
       </main>
       <Footer />
     </div>
