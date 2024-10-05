c83645ba06b8b7c8ca85e71e173200b8741a13c7
Matheus P Agostinho <contact@matheuspa.com>
2024-10-05 18:12:53 -0300

    finished fetch data profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,29 @@
-import getUser from "@/actions/auth/profile";
+'use client'
 
-export default async function Profile() {
-  const user = await getUser();
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+export async function getUser() {
+  const response = await fetch("/api/game/profile");
+  const user = await response.json();
+  return user;
+}
+
+export default function Profile() {
+  const router = useRouter();
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    ; (async () => {
+      const d = await getUser()
+      if (d.error) {
+        router.push("/login");
+      }
+      setData(d)
+    })();
+  }, [])
+
   return <main className="bg-background-gradient h-dvh grid place-content-center">
-    <pre>{JSON.stringify(user, null, 2)}</pre>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
   </main>
 }
