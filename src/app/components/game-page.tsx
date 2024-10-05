75fc6697eb7311ffac9b2e05fe85dd6d81913c1d
Matheus P Agostinho <contact@matheuspa.com>
2024-10-05 18:10:53 -0300

    change style page

--- src/app/components/game-page.tsx
+++ src/app/components/game-page.tsx
@@ -2,7 +2,7 @@
 
 import { GameProvider, useGame } from "@/context/game-context";
 import Loading from "@/app/loading";
-import { DefeatDialog, Footer, HowToPlay, Keyboard, Row, WinDialog } from "@/components";
+import { DefeatDialog, HowToPlay, Keyboard, Row, WinDialog } from "@/components";
 
 export function GamePage() {
   return <GameProvider>
@@ -30,56 +30,54 @@ export function GameContainer() {
   if (!correctWord) return <Loading />
 
   return (
-    <div className="flex flex-col items-center justify-between p-4">
-      <div className="flex flex-col gap-8 justify-between md:justify-start pb-8 h-full sm:h-screen">
-        <header className="flex flex-col gap-1 items-center">
-          <h1 className="text-2xl font-bold text-center text-foreground">Guess the word</h1>
-          <p className="text-center text-foreground/80 text-xl">
-            You have {' '}
-            <code
-              className="py-1 px-2 aspect-square text-foreground dark:text-white bg-gray-300 dark:bg-gray-800 rounded-md font-semibold"
-            >
-              {maxAttempts - currentAttempt}
-            </code>
-            {' '} attempts left
-          </p>
-          <p className="flex flex-row gap-1 text-foreground/80"><dt className="font-bold">Tip:</dt> <dd>{correctWord.tip ?? ''}</dd></p>
-          <HowToPlay />
-        </header>
+    <div className="flex flex-col gap-8 justify-start items-center p-4">
+      <header className="flex flex-col gap-1 items-center">
+        <h1 className="text-2xl font-bold text-center text-foreground">Guess the word</h1>
+        <p className="text-center text-foreground/80 text-xl">
+          You have {' '}
+          <code
+            className="py-1 px-2 aspect-square text-foreground dark:text-white bg-gray-300 dark:bg-gray-800 rounded-md font-semibold"
+          >
+            {maxAttempts - currentAttempt}
+          </code>
+          {' '} attempts left
+        </p>
+        <p className="flex flex-row gap-1 text-foreground/80"><dt className="font-bold">Tip:</dt> <dd>{correctWord.tip ?? ''}</dd></p>
+        <HowToPlay />
+      </header>
 
-        <WinDialog
-          open={win}
-          data={{ attempts: maxAttempts - currentAttempt }}
-          onNextRound={resetTurn}
-        />
+      <WinDialog
+        open={win}
+        data={{ attempts: maxAttempts - currentAttempt }}
+        onNextRound={resetTurn}
+      />
 
-        <DefeatDialog
-          open={defeat}
-          onTryAgain={resetTurn}
-        />
+      <DefeatDialog
+        open={defeat}
+        onTryAgain={resetTurn}
+      />
 
-        <main className="max-w-2xl min-h-fit flex flex-col w-full space-y-4">
-          {gameWords.map((word: string, index: number) => (
-            <Row
-              focused={index === currentAttempt && !win}
-              key={`row-${index}`}
-              word={index === currentAttempt ? currentWord : word}
-              size={wordSize}
-              correctWord={correctWord.word}
-              checkWord={canCheck[index]}
-            />
-          ))}
-        </main>
-        <Keyboard
-          className="mb-6"
-          disabled={defeat || win}
-          onKeyPress={handleWordChange}
-          onBackspace={handleBackspace}
-          wordSize={wordSize}
-          onConfirm={handleConfirm}
-        />
-      </div>
-      <Footer />
+      <main className="max-w-2xl min-h-fit flex flex-col w-full space-y-4">
+        {gameWords.map((word: string, index: number) => (
+          <Row
+            focused={index === currentAttempt && !win}
+            key={`row-${index}`}
+            word={index === currentAttempt ? currentWord : word}
+            size={wordSize}
+            correctWord={correctWord.word}
+            checkWord={canCheck[index]}
+          />
+        ))}
+      </main>
+      <Keyboard
+        className="mb-6"
+        disabled={defeat || win}
+        onKeyPress={handleWordChange}
+        onBackspace={handleBackspace}
+        wordSize={wordSize}
+        onConfirm={handleConfirm}
+      />
     </div>
+
   );
 }
