1ef0c55753b33b66ee85e8299c878633161adb87
Matheus P Agostinho <contact@matheuspa.com>
2024-10-07 15:14:50 -0300

    added model for handle alphabet datas

---
+++ src/model/alphabet.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/db/prisma";
+import { Alphabet } from "@prisma/client";
+
+const getAlphabets = async () => {
+  const data = await prisma.alphabet.findMany();
+  return data;
+};
+
+const getAlphabet = async (name: string): Promise<Alphabet | null> => {
+  const data = await prisma.alphabet.findUnique({
+    where: {
+      name,
+    },
+  });
+  return data;
+};
+
+const createAlphabet = async (
+  name: string,
+  characters: string
+): Promise<Alphabet> => {
+  const alphabet = await prisma.alphabet.create({
+    data: {
+      name,
+      characters,
+    },
+  });
+
+  return alphabet;
+};
+
+export { getAlphabets, getAlphabet, createAlphabet };
