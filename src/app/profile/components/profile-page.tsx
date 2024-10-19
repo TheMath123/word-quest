88d0fc76d3884860d9b32ac4e5e0915ebbecd3d8
Matheus P Agostinho <contact@matheuspa.com>
2024-10-18 22:12:28 -0300

    chore design profiel, added total completed

--- src/app/profile/components/profile-page.tsx
+++ src/app/profile/components/profile-page.tsx
@@ -3,7 +3,7 @@ import { Role } from "@/@types/role"
 import { ProfilePhoto } from "@/components/profile/profile-photo"
 import { getProfile } from "@/services/auth"
 import Link from "next/link"
-import { WinkGhost } from '@mynaui/icons-react';
+import { CheckCircle } from '@mynaui/icons-react';
 import { searchGameData } from "@/services/game-data";
 
 
@@ -21,8 +21,8 @@ export async function ProfilePage() {
 
   const gameData = user?.id ? await searchGameData(user.id) : null
 
-  return <main className="bg-background-gradient h-dvh p-4">
-    <header className="flex flex-row justify-between">
+  return <main className="bg-background-gradient h-dvh">
+    <header className="flex flex-row justify-between p-4">
       <aside className="flex flex-row gap-4 items-center">
         {user && user.image ?
           <ProfilePhoto
@@ -32,8 +32,8 @@ export async function ProfilePage() {
           />
           : null}
         <div>
-          <h1 className="text-2xl font-medium">Welcome, <span className="font-semibold">{user!.name}</span></h1>
-          <h2 className="text-base font-light">{user!.email}</h2>
+          <h1 className="text-base md:text-2xl font-medium">Welcome, <span className="font-semibold whitespace-nowrap">{user!.name}</span></h1>
+          <h2 className="text-sm md:text-base font-light">{user!.email}</h2>
         </div>
       </aside>
       <nav className="flex gap-4 p-4">
@@ -44,9 +44,17 @@ export async function ProfilePage() {
         </ul>
       </nav>
     </header>
-    <section className="py-8 grid place-content-center pt-20">
-      <WinkGhost className="w-16 h-16" />
-      <pre>{JSON.stringify(gameData, null, 2)}</pre>
+    <section className="py-8 grid px-4 md:px-24">
+      <div className="border border-gray-900 dark:border-gray-100 rounded-md p-4 flex flex-col gap-2 items-center w-fit">
+        <h1 className="font-medium text-xl">
+          Total Puzzles Completed
+        </h1>
+        <div className="flex gap-2 items-center">
+          <CheckCircle className="w-5 h-5 stroke-green-500" />
+          <span className="font-semibold text-2xl">{gameData!.totalCompleted}</span>
+        </div>
+      </div>
+      {/* <pre>{JSON.stringify(gameData, null, 2)}</pre> */}
     </section>
   </main>
 }
