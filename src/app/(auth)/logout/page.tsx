8f58d399b824c1cb748bfcd87af1c5bbaa90e411
Matheus P Agostinho <contact@matheuspa.com>
2024-10-24 16:21:21 -0300

    change logout page

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -4,24 +4,28 @@ import { logout } from "@/services/auth/logout";
 import { Footer } from "@/components";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Logout() {
   const router = useRouter()
   useEffect(() => {
     const leave = async () => {
       await logout();
-      router.replace('/');
+      router.push('/');
     };
 
     leave();
   }, []);
 
   return (
-    <div className="flex flex-col h-dvh">
+    <main className="flex flex-col justify-between items-center h-dvh">
+      <nav className="w-full p-4 flex justify-end">
+        <Link href='/' className="font-medium">Home</Link>
+      </nav>
       <main className="grid place-content-center h-full">
         <h1 className="text-2xl font-bold">Good bye! =)</h1>
       </main>
       <Footer />
-    </div>
+    </main>
   );
 }
